e125e7b4c55e16c3266b00af1094160988b06541
karayanni <nader.karayanni@gmail.com>
2024-05-05 22:48:46 -0400

    last chance

--- scraping_test.py
+++ scraping_test.py
@@ -2,6 +2,8 @@ from seleniumbase import Driver
 from bs4 import BeautifulSoup
 from seleniumbase.undetected import ChromeOptions
 from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 
 
 ua = UserAgent()
@@ -11,8 +13,18 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument(user_agent)
-driver = Driver(browser="chrome", headless=True, agent=user_agent, no_sandbox=True, disable_gpu=True)
+driver = Driver(browser="chrome", headless=True, agent="user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", no_sandbox=True, disable_gpu=True)
 driver.get("https://www.reddit.com/search/?q=dog+food")
+
+options = ChromeOptions()
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+
+options.binary_location = '/home/sundaiclub/agent-x/venv/lib/python3.11/site-packages/seleniumbase/drivers/chromedriver'
+driver = webdriver.Chrome(options=options)
 
 html_content = driver.page_source
 soup = BeautifulSoup(html_content, 'html.parser')
