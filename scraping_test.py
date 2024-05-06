be71c04b1b6b7d7e3ecd2db9502e6a0779ddafa4
karayanni <nader.karayanni@gmail.com>
2024-05-05 23:06:47 -0400

    last chance

--- scraping_test.py
+++ scraping_test.py
@@ -4,28 +4,30 @@ from seleniumbase.undetected import ChromeOptions
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-ua = UserAgent()
-user_agent = ua.random
-options = ChromeOptions()
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument(user_agent)
-driver = Driver(browser="chrome", headless=True, agent="user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", no_sandbox=True, disable_gpu=True)
-driver.get("https://www.reddit.com/search/?q=dog+food")
+# ua = UserAgent()
+# user_agent = ua.random
+# options = ChromeOptions()
+# options.add_argument("--window-size=1920,1080")
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+# options.add_argument(user_agent)
+# driver = Driver(browser="chrome", headless=True, agent="user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", no_sandbox=True, disable_gpu=True)
+# driver.get("https://www.reddit.com/search/?q=dog+food")
+#
+# options = ChromeOptions()
+# options.add_argument("--window-size=1920,1080")
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+# options.add_argument(
+#     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+#
+# options.binary_location = '/home/sundaiclub/agent-x/venv/lib/python3.11/site-packages/seleniumbase/drivers/chromedriver'
+# driver = webdriver.Chrome(options=options)
 
-options = ChromeOptions()
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-
-options.binary_location = '/home/sundaiclub/agent-x/venv/lib/python3.11/site-packages/seleniumbase/drivers/chromedriver'
-driver = webdriver.Chrome(options=options)
-
+driver = webdriver.Chrome(ChromeDriverManager().install())
 html_content = driver.page_source
 soup = BeautifulSoup(html_content, 'html.parser')
 
