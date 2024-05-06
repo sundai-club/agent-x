eec0673458786d20c2c335de3e747db6da992377
karayanni <nader.karayanni@gmail.com>
2024-05-05 21:45:24 -0400

    fake user agent

--- scraping_test.py
+++ scraping_test.py
@@ -1,13 +1,16 @@
 from seleniumbase import Driver
 from bs4 import BeautifulSoup
 from seleniumbase.undetected import ChromeOptions
+from fake_useragent import UserAgent
 
+
+ua = UserAgent()
+user_agent = ua.random
 options = ChromeOptions()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+options.add_argument(user_agent)
 driver = Driver(browser="chrome", headless=True, agent="user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 driver.get("https://www.reddit.com/search/?q=dog+food")
 
