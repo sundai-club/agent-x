4e21003f009cb58e15a990ffe71b2fcbd9a93ffa
karayanni <nader.karayanni@gmail.com>
2024-05-05 23:13:06 -0400

    last chance2.0 haha

--- scraping_test.py
+++ scraping_test.py
@@ -1,10 +1,8 @@
-from seleniumbase import Driver
 from bs4 import BeautifulSoup
-from seleniumbase.undetected import ChromeOptions
-from fake_useragent import UserAgent
 from selenium import webdriver
-from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ChromeOptions
 
 
 # ua = UserAgent()
@@ -15,19 +13,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 # options.add_argument("--disable-gpu")
 # options.add_argument(user_agent)
 # driver = Driver(browser="chrome", headless=True, agent="user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", no_sandbox=True, disable_gpu=True)
-# driver.get("https://www.reddit.com/search/?q=dog+food")
 #
-# options = ChromeOptions()
-# options.add_argument("--window-size=1920,1080")
-# options.add_argument("--headless")
-# options.add_argument("--disable-gpu")
-# options.add_argument(
-#     "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-#
+options = ChromeOptions()
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+
 # options.binary_location = '/home/sundaiclub/agent-x/venv/lib/python3.11/site-packages/seleniumbase/drivers/chromedriver'
 # driver = webdriver.Chrome(options=options)
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+driver.get("https://www.reddit.com/search/?q=dog+food")
+
+
 html_content = driver.page_source
 soup = BeautifulSoup(html_content, 'html.parser')
 
